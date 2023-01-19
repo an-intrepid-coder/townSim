9628083736945110a6e5d963ca07b354dfe7f774
Sam Gibson <sgibber2018@gmail.com>
2023-01-19 00:20:56 -0800

    terrain generation -- forests

--- src/modules/gameState.mjs
+++ src/modules/gameState.mjs
@@ -21,13 +21,43 @@ export class GameState {
                 this.cells.push(new Cell(x, y));
             }
         }
-        // forests TODO
-        // lakes TODO
-        // river(s) TODO
+        this.stampForests();
+        this.stampLakes();
+        this.stampRivers();
     }
 
     stampForests() {
-        // TODO using a cellular automata
+        for (let y = 0; y < this.mapCellsHigh; y++) {
+            for (let x = 0; x < this.mapCellsWide; x++) {
+                let index = y * this.mapCellsWide + x;
+                let isForest = Math.floor(Math.random() * 10) % 2 == 0;
+                if (isForest) {
+                    this.cells[index].terrain = "trees";
+                }
+            }
+        }    
+        let generations = 1;
+        let threshold = 5;
+        for (let i = 0; i < generations; i++) {
+            for (let y = 0; y < this.mapCellsHigh; y++) {
+                for (let x = 0; x < this.mapCellsWide; x++) {
+                    let index = y * this.mapCellsWide + x;
+                    let count = 0;
+                    let neighbors = this.getCellNeighbors(x, y);
+                    for (let cell of neighbors) {
+                        if (cell.terrain == "trees") {
+                            count++;
+                        }
+                    }
+                    let cell = this.cells[index];
+                    if (count >= threshold) {
+                        cell.terrain = "trees";
+                    } else {
+                        cell.terrain = "unset";
+                    }
+                }
+            }
+        }
     }
 
     stampLakes() {
@@ -40,6 +70,19 @@ export class GameState {
 
     getCellTerrain(x, y) {
         return this.cells[y * this.mapCellsWide + x].terrain;
+    }
+
+    getCellNeighbors(x, y) {
+        let neighbors = [];
+        for (let i = y - 1; i < y + 2; i++) {
+            for (let j = x - 1; j < x + 2; j++) { 
+                if (i > 0 && j > 0 && i < this.mapCellsHigh && j < this.mapCellsWide) {
+                    let index = i * this.mapCellsWide + j;
+                    neighbors.push(this.cells[index]);
+                }
+            }
+        }
+        return neighbors;
     }
 }
 
